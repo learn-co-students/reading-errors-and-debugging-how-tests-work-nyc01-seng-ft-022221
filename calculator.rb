18ac4f9250f41ef2058e750192191a69275f2002
Andre Powers <andre@stuypend.com>
2021-02-03 15:57:45 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
+first_number = 4
 # Add your variables here
+second_number = 6
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number*second_number
+quotient = first_number/second_number
