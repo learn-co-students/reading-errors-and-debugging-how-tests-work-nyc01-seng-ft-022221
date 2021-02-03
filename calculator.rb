6e944e5e67029c438f925668322556d643892154
Barak Rosner <rosnerbb@gmail.com>
2021-02-03 21:54:39 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
-# Add your variables here
+first_number = 1 
+
+second_number = 3
+
+sum = first_number + second_number
+sum
+
+difference = first_number - second_number
+difference
+
+product = first_number * second_number
+product 
+
+quotient = first_number / second_number
+quotient
